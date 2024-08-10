7465b331945fb048dd99eb24d2acd534edec31fd
sahil-lalani <slalani304@gmail.com>
2024-08-10 19:07:41 -0400

    way better linkedin exporting

--- src/main/Companies/Microsoft/linkedin.js
+++ src/main/Companies/Microsoft/linkedin.js
@@ -11,9 +11,76 @@ async function exportLinkedin() {
 
   profileButton.click();
   await new Promise((resolve) => setTimeout(resolve, 2000));
-  const linkedinProfile = document.body.outerHTML;
-  customConsoleLog('linkedin done!');
-  return linkedinProfile;
+  
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      const sections = document.querySelectorAll(
+        "section[data-view-name='profile-card']",
+      );
+      const contactBtn = document.getElementById(
+        'top-card-text-details-contact-info',
+      );
+      if (sections.length > 5 && contactBtn) {
+        customConsoleLog('GOT SECTIONS + contact btn!');
+        clearInterval(interval);
+        const mainContent = document.querySelector('.scaffold-layout__main');
+        if (mainContent) {
+          contactBtn.click();
+
+          // wait for this class to work for document.querySelector: pv-contact-info__contact-type
+          let email = '';
+          const waitForContactInfo = setInterval(() => {
+            const contactInfoElements = document.getElementsByClassName(
+              'pv-contact-info__contact-type',
+            );
+            if (contactInfoElements.length > 0) {
+              clearInterval(waitForContactInfo);
+
+              // Loop through each element
+              Array.from(contactInfoElements).forEach((element) => {
+                // Check for anchor tags to get links
+                const links = element.getElementsByTagName('a');
+                if (links.length > 0) {
+                  Array.from(links).forEach((link) => {
+                    if (link.href.includes('mailto:')) {
+                      email = link.href.replace('mailto:', '');
+                      customConsoleLog('got email: ', email);
+                    }
+                  });
+                }
+              });
+
+              const profileData = {
+                name: document.querySelector('h1')?.innerText || '',
+                subheading:
+                  document.querySelector('.text-body-medium.break-words')
+                    ?.innerText || '',
+                about: (
+                  document
+                    .querySelector(
+                      "section[data-view-name='profile-card'] div#about",
+                    )
+                    ?.closest('section')?.innerText || ''
+                ).replace(/^About\nAbout\n/, ''),
+                profile_url: window.location.href,
+                experience: (
+                  document
+                    .querySelector(
+                      "section[data-view-name='profile-card'] div#experience",
+                    )
+                    ?.closest('section')?.innerText || ''
+                ).replace(/^Experience\nExperience\n/, ''),
+                email: email || '',
+              };
+              customConsoleLog('sending back profile data!');
+              customConsoleLog('linkedin done!');
+              resolve(profileData);
+            }
+          }, 100);
+        }
+      }
+    }, 100);
+  });
 }
 
 module.exports = exportLinkedin;
