a554bd78b028440476a788ee110e476937ffb7e0
sahil-lalani <slalani304@gmail.com>
2024-08-21 21:56:58 -0400

    local working now!

--- analytics.js
+++ analytics.js
@@ -1,23 +1,31 @@
-import config from './config.json'
 import { createClient } from '@supabase/supabase-js';
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient(config.supabase_url, config.supabase_key);
+let supabase;
+let config;
+
+
+  try {
+    config = require('./config.json');
+    supabase = createClient(config.supabase_url, config.supabase_key);
+  } catch (err) {
+    console.error('No analytics config or supabase client found:', err);
+  }
+
+
 
 export async function trackRun(status, company, name, lastStep) {
-
-  const { data, error } = await supabase
-    .from('runs')
-    .insert([
-      {
-        timestamp: new Date().toISOString(),
-        status: status,
-        company: company,
-        name: name,
-        environment: process.env.NODE_ENV === 'production' ? 'production' : 'local',
-        lastStep: lastStep ? lastStep : JSON.stringify({})
-      },
-    ]);
+  if (!config || !supabase) return;
+  const { data, error } = await supabase.from('runs').insert([
+    {
+      timestamp: new Date().toISOString(),
+      status: status,
+      company: company,
+      name: name,
+      environment:
+        process.env.NODE_ENV === 'production' ? 'production' : 'local',
+      lastStep: lastStep ? lastStep : JSON.stringify({}),
+    },
+  ]);
 
   if (error) {
     console.error('Error writing to Supabase:', error);
